f79bb21ef350759d946dc79a067e237828bb7e3b
Akram Samir <akram.sameer99@gmail.com>
2022-07-29 18:24:04 +0200

    WIP
    - Adding schame and access control
    - Finish register for clients

--- src/components/user/controller.ts
+++ src/components/user/controller.ts
@@ -1,15 +1,44 @@
 import { RequestHandler } from 'express';
-import bcrypt from 'bcryptjs';
-import { User } from './model';
+import db from '../../dbClient';
 import logger from '../../loaders/logger';
 
+import { Roles } from '../../constants';
 import { appResponse } from '../../utils';
+import UserService from './service';
 
-const registerUser: RequestHandler = async (req, res) => {
+// only client will use this method to create a new register
+const registerClient: RequestHandler = async (req, res) => {
   try {
-    const { username, password, email, name } = req.body;
+    const { password, email, firstName, lastName, company } = req.body;
 
-    res.status(201).send({});
+    // get user with email
+    const userExist = await db.user.findFirst({
+      where: { email: { equals: email } },
+    });
+
+    // check if there is a user with the same email
+    if (userExist)
+      return res.status(400).send(appResponse('Email exist before.', false));
+
+    // creating a clientInfo object
+    const createdClientInfo = await db.clientInfo.create({
+      data: { company },
+    });
+
+    const hashedPassword = await UserService.getHashedPassword(password);
+
+    const createdUser = await db.user.create({
+      data: {
+        email,
+        hashedPassword,
+        firstName,
+        lastName,
+        role: Roles.CLIENT,
+        clientInfoId: createdClientInfo.id,
+      },
+    });
+
+    res.status(201).send({ createdUser });
   } catch (err) {
     logger.error(err);
 
@@ -32,4 +61,4 @@ const loginUser: RequestHandler = async (req, res) => {
   }
 };
 
-export { registerUser };
+export { registerClient };
