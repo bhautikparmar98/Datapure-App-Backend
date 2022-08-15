95cc92c92023ac2ac1595f3638805368499c17b5
Akram Samir <akram.sameer99@gmail.com>
2022-08-15 20:50:13 +0200

    trying

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 import { Db } from 'mongodb';
 import config from '../config';
+import LoggerInstance from './logger';
 
-export default async (): Promise<Db> => {
-  const connection = await mongoose.connect(config.mongoDatabaseURL);
-  return connection.connection.db;
+export default async (): Promise<any> => {
+  try {
+    const connection = await mongoose.connect(config.mongoDatabaseURL);
+    return connection.connection.db;
+  } catch (error) {
+    LoggerInstance.log('error', 'Unable to connect with mongo');
+  }
 };
