b8915cfe6e6b67724213fc538a1ffcebb1f19023
Mahmoud Bekheet <mahmoud.a.bekheet@gmail.com>
2022-10-22 07:51:36 +0200

    Add nginx config and change express body parser

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import Logger from './loaders/logger';
 
 //expand the body limit
-app.use(express.json({ limit: '50mb' }));
+// app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // make app listen to the port that in the config
