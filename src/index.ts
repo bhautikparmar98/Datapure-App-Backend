416de5f4423256818c1885abe950dfa5337098ea
Mahmoud Bekheet <mahmoud.a.bekheet@gmail.com>
2022-10-22 17:21:47 +0200

    change nginx and express limits

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import Logger from './loaders/logger';
 
 //expand the body limit
 // app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // make app listen to the port that in the config
 app.listen(config.application.port, () => {
